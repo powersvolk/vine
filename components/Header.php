8b23b52ed5f6c44b9abf0cbb8ab18ec9de375e25
Vladimir <44676472+powersvolk@users.noreply.github.com>
2024-04-29 10:43:36 +0300

    Prepare

--- components/Header.php
+++ components/Header.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic&display=swap" rel="stylesheet" />
 </head>
 <body>
     
-<div class="header">
-    1
-</div>
+ 
