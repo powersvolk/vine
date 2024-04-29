ed2d17c6c07bd02b23bebfe1d31b487f2233c55c
Vladimir <44676472+powersvolk@users.noreply.github.com>
2024-04-29 18:27:56 +0300

    Header

--- components/Header.php
+++ components/Header.php
@@ -9,4 +9,74 @@
 </head>
 <body>
     
- 
+<section class="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="header__inner-logo">
+                <a href="#">
+                    <img src="img/logo.svg" alt="Vince Vine">
+                </a>
+            </div>
+            <div class="header__inner-catalog">
+                <a href="#" class="cta cta-catalog">
+                    <svg class="icon">
+                        <use xlink:href="#catalog__icon"></use>
+                    </svg>
+                   Каталог 
+                </a>
+            </div>
+            <div class="header__inner-search">
+                <form>
+                    <input type="text" placeholder="Название товара, бренда, артикул ...">
+                    <input type="submit" value="">
+                </form>
+            </div>
+            <div class="header__inner-work">
+                <div class="header__inner-phone">
+                    +7 (499) 113-22-24
+                </div>
+                <div class="header__inner-time">
+                    Пн-Вс 10:00 до 21:00
+                    <a href="#">
+                        <svg class="icon">
+                            <use xlink:href="#viber"></use>
+                        </svg>
+                    </a>
+                    <a href="#">
+                        <svg class="icon">
+                            <use xlink:href="#telegram"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="header__inner-other">
+                <div class="header__inner-other-burger">
+                    <div class="burger_line"></div>
+                    <div class="burger_line"></div>
+                    <div class="burger_line"></div>
+                </div>
+                <div class="header__inner-other-profile">
+                    <a href="#">
+                        <svg class="icon">
+                            <use xlink:href="#user"></use>
+                        </svg>
+                    </a>
+                </div>
+                <div class="header__inner-other-compare">
+                    <a href="#">
+                        <svg class="icon">
+                            <use xlink:href="#heart"></use>
+                        </svg>
+                    </a>
+                </div>
+                <div class="header__inner-other-cart">
+                    <a href="#">
+                        <svg class="icon">
+                            <use xlink:href="#cart"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>   
+        </div>
+    </div>
+</section>    
