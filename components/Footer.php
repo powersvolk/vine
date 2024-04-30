c7ea581d61053ba3eb1497a2ca7a0eb1e68bd95f
Vladimir <44676472+powersvolk@users.noreply.github.com>
2024-04-30 17:46:22 +0300

    Hero banner & category

--- components/Footer.php
+++ components/Footer.php
@@ -238,6 +238,7 @@
 
 <?php include('components/SVG/SvgSprite.php'); ?>
 <script src="/js/jquery-3.7.1.min.js"></script>
+<script src="/js/owl.carousel.min.js"></script>
 <script src="/js/main.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include('components/Header.php'); ?>
-
+<?php include('components/BannerPromotion.php'); ?>
+<?php include('components/CategoryIndex.php'); ?>
 <?php include('components/Footer.php'); ?>
